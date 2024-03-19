61117acb95f608a584a8861f2287be4f397bb1df
mehdi <mehdi.alouane@laplateforme.io>
2024-03-19 14:42:43 +0100

    Mise en place de 1 a la place des emplacements vide

--- sudokU.py
+++ sudokU.py
@@ -1,18 +1,22 @@
 import numpy as np
+
 with open("t.txt") as my_file:
-    tabl =[]
     tabl = my_file.read()
+
 tabl1 = []
-t=0
-tabl2=[]
 for i in tabl:
-    if i!= '\n' :
+    if i != '\n':
         tabl1.append(i)
-for i in range (0,9):
+
+tabl2 = []
+for i in range(0, 9):
     tabl2.append(tabl1[0:9])
     del tabl1[0:9]
 
-for i in tabl2:
-    if  i !='_':
-        print(i)
-    
+for i in range(len(tabl2)):
+    for j in range(len(tabl2[i])):
+        if tabl2[i][j] == '_':
+            tabl2[i][j] = '1'
+
+for row in tabl2:
+    print(row)
