2cbee740adbf45155f9d410c88f2012558c32cf0
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-03-19 15:34:51 +0100

    creation de la liste de liste

--- test.py
+++ test.py
@@ -2,12 +2,30 @@ class Sudoku:
     
     def __init__(self) :
         #read sudoku.txt to have the sudoku
-        with open("sudoku.txt", "r") as f:
-            self.grid = f.read()  
         #same but we'll have line per line for row
         with open("sudoku.txt", "r") as f:
             self.row = f.readlines()
+        with open("sudoku.txt") as my_file:
+            self.grid = my_file.read()
+
+            self.grid1 = []
+            for i in self.grid:
+                if i != '\n':
+                    self.grid1.append(i)
+
+            self.grid2 = []
+            for i in range(0, 9):
+                self.grid2.append(self.grid1[0:9])
+                del self.grid1[0:9]
+
+            for i in range(len(self.grid2)):
+                for j in range(len(self.grid2[i])):
+                    if self.grid2[i][j] == '_':
+                        self.grid2[i][j] = '0'        
     
+    def createGrid(self):
+        print(self.grid2)
+
     def printGrid(self):
         print(self.grid)
     
@@ -20,16 +38,41 @@ class Sudoku:
         #take the column thank to the indice of each number
         column= []
         for i in range(0,9):
-            column.append(self.row[i][0])
-        print(column[3])    
+            column.append(self.row[i][number-1])
+        return column    
     
-    def verifRow(self):
+    #method to verif the entities in a row
+    def verifRow(self,number):
         # variable n to know wich case the program verif
         n=0
         # variable n to know when the program finish its verif
         end_verif=0
         #for each number in a row , we look if it's a number or not and what is the number
-        for i in self.returnRow(1):
+        for i in self.returnRow(number):
+            n+=1
+            numberHere=False
+            for j in range(1,10):
+                end_verif+=1
+                if end_verif==9:
+                    end_verif=0
+                    if i == f"{j}":
+                        print(f"num : {i} in case {n}")
+                        numberHere=True
+                    else:
+                        if numberHere == False:
+                            print(f"not num in case {n}")
+                elif i == f"{j}":
+                    print(f"num : {i} in case {n}")
+                    numberHere=True
+
+    #same method to verif but in column
+    def verifColumn(self,number):
+        # variable n to know wich case the program verif
+        n=0
+        # variable n to know when the program finish its verif
+        end_verif=0
+        #for each number in a row , we look if it's a number or not and what is the number
+        for i in self.returnColumn(number):
             n+=1
             numberHere=False
             for j in range(1,10):
@@ -46,9 +89,9 @@ class Sudoku:
                     print(f"num : {i} in case {n}")
                     numberHere=True
     
-    # def verifColumn(self):
-
 
     
 sudo=Sudoku()
 sudo.returnColumn(1)
+sudo.verifColumn(1)
+sudo.createGrid()
---
+++ test2.py
@@ -0,0 +1,67 @@
+def est_valide(grille, ligne, colonne, nombre):
+    # Vérifie si le nombre est valide dans la ligne
+    if nombre in grille[ligne]:
+        return False
+
+    # Vérifie si le nombre est valide dans la colonne
+    for i in range(9):
+        if grille[i][colonne] == nombre:
+            return False
+
+    # Vérifie si le nombre est valide dans le bloc 3x3
+    bloc_ligne = ligne // 3 * 3
+    bloc_colonne = colonne // 3 * 3
+    for i in range(3):
+        for j in range(3):
+            if grille[bloc_ligne + i][bloc_colonne + j] == nombre:
+                return False
+
+    return True
+
+def trouver_case_vide(grille):
+    # Trouve la prochaine case vide dans la grille
+    for i in range(9):
+        for j in range(9):
+            if grille[i][j] == 0:
+                return i, j
+    return None, None  # Aucune case vide trouvée
+
+def resoudre_sudoku(grille):
+    ligne, colonne = trouver_case_vide(grille)
+    if ligne is None:
+        return True  # Toutes les cases sont remplies, le Sudoku est résolu
+
+    # Essayer différentes valeurs pour la case vide
+    for nombre in range(1, 10):
+        if est_valide(grille, ligne, colonne, nombre):
+            grille[ligne][colonne] = nombre
+            if resoudre_sudoku(grille):
+                return True
+            grille[ligne][colonne] = 0  # Backtrack si la solution n'est pas valide
+
+    return False  # Aucune valeur ne convient, la grille est invalide
+
+def afficher_grille(grille):
+    for ligne in grille:
+        print(" ".join(map(str, ligne)))
+
+# Exemple de grille Sudoku à résoudre (0 représente une case vide)
+grille = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Grille Sudoku à résoudre :")
+afficher_grille(grille)
+print("\nSolution :")
+if resoudre_sudoku(grille):
+    afficher_grille(grille)
+else:
+    print("Aucune solution n'est possible.")
