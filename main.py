c6ea9696350d2590190b4c40b9c9bca8106ee131
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-03-20 11:55:37 +0100

    Création fichier 'main.py' pour naviguer dans la grille

---
+++ main.py
@@ -0,0 +1,31 @@
+class Sudoku():
+
+        with open("sudoku1.txt") as my_file:
+            tabl = my_file.read()
+
+        tabl1 = []
+        for i in tabl:
+            if i != '\n':
+                tabl1.append(i)
+
+        # print(tabl1)
+
+        tabl2 = []
+        for i in range(0, 9):
+            tabl2.append(tabl1[0:9])
+            del tabl1[0:9]
+
+        emplacements_vides = []
+
+        for i in range(9):
+            for j in range(9):
+                if tabl2[i][j] == '_':
+                    emplacements_vides.append((i, j))
+
+        print(tabl2)
+
+
+        # print("Emplacements vides:")
+        # for emplacement in emplacements_vides:
+        #     print(emplacement)
+
