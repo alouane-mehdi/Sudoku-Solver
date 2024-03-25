7b19e87a23bfef0694792dcf528437ebd8975724
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-03-25 12:22:48 +0100

    continuation du code qui verifie si le sudoku complet est valide

--- teeeest copy.py
+++ teeeest copy.py
@@ -1,37 +1,45 @@
+
+class Grid:
+    def __init__(self) -> None:
+        with open("evilsudoku.txt") as my_file:
+            content = my_file.readlines()
+        grid = [list(line.strip()) for line in content]
+           
 class Verif:
-    def __init__(self, filename="evilsudoku.txt"):
-        with open(filename) as my_file:
+    def __init__(self) -> None:
+        
+        with open("evilsudoku.txt") as my_file:
             content = my_file.readlines()
 
         self.grid = [list(line.strip()) for line in content]
 
-    def is_row_valid(self, row):
+    def isRowValid(self,grid, row):
         """
-        Check if the row is valid.
+        look at if the row is valid
         """
         num = set()
         for col in range(9):
             if self.grid[row][col] in num:
                 return False
-            if self.grid[row][col] != '0':
+            if self.grid[row][col] != 0:
                 num.add(self.grid[row][col])
         return True
 
-    def is_col_valid(self, column):
+    def isColValid(self,grid, column):
         """
-        Check if the column is valid.
+        look at if the column is valid
         """
         num = set()
         for row in range(9):
             if self.grid[row][column] in num:
                 return False
-            if self.grid[row][column] != '0':
+            if self.grid[row][column] != 0:
                 num.add(self.grid[row][column])
         return True
 
-    def is_square_valid(self, square_row, square_column):
+    def isSquareValid(self,grid, square_row, square_column):
         """
-        Check if the 3x3 square is valid.
+        look at if the square 3x3 is valid
         """
         num = set()
         for row in range(3):
@@ -39,40 +47,25 @@ class Verif:
                 val = self.grid[square_row * 3 + row][square_column * 3 + column]
                 if val in num:
                     return False
-                if val != '0':
+                if val != 0:
                     num.add(val)
         return True
-    
-    def is_grid_full(self):
-        for i in range(9):
-            for j in range(9):
-                if self.grid[i][j] == '_' :
-                    print("no")
-                return False
 
-    def is_grid_valid(self):
+    def isGridValid(self,grid):
         """
-        Check if the entire grid is valid.
+        look at if the grid is valid
         """
-
-        if self.is_grid_full():
-            print("no")
-            return False
-        
         for i in range(9):
-            if not self.is_row_valid(i) or not self.is_col_valid(i):
-                print("no")
+            if not self.isRowValid(self.grid, i) or not self.isColValid(self.grid, i):
                 return False
 
         for i in range(3):
             for j in range(3):
-                if not self.is_square_valid(i, j):
+                if not self.isSquareValid(self.grid, i, j):
                     print("no")
                     return False
 
         return True
 
-verif=Verif()
-verif.is_grid_valid()
-
-     
+yeah=Verif()
+yeah.isGridValid(Grid.grid)
