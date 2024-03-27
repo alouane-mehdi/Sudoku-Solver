5f53dbf103c60fc0d9f3c66c847e84c9c0ba2c3e
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-03-27 14:44:48 +0100

    suppression de la class verification car je les fusionner dans la class sudoku

--- Bruteforce.py
+++ Bruteforce.py
@@ -1,15 +1,96 @@
 from Verification import Verif
-from Grid import Grid
-
+import random
 
 class Sudoku:
     
     def __init__(self) :
-        gridClass=Grid()
-        self.verif=Verif()
+        with open("evilsudoku.txt", "r") as f:
+            row = f.readlines()
+        with open("evilsudoku.txt") as my_file:
+            liste = my_file.read()
 
-        self.grid = gridClass.grid()
+            liste1 = []
+            for i in liste:
+                if i != '\n':
+                    liste1.append(i)
 
+            self.grid = []
+            for i in range(0, 9):
+                self.grid.append(liste1[0:9])
+                del liste1[0:9]
+
+            for i in range(len(self.grid)):
+                for j in range(len(self.grid[i])):
+                    if self.grid[i][j] == '_':
+                        self.grid[i][j] = '0'
+
+    def is_row_valid(self, row):
+        """
+        Check if the row is valid.
+        """
+        num = set()
+        for col in range(9):
+            if self.grid[row][col] in num:
+                return False
+            if self.grid[row][col] != '_':
+                num.add(self.grid[row][col])
+        return True
+
+    def is_col_valid(self, column):
+        """
+        Check if the column is valid.
+        """
+        num = set()
+        for row in range(9):
+            if self.grid[row][column] in num:
+                return False
+            if self.grid[row][column] != '_':
+                num.add(self.grid[row][column])
+        return True
+
+    def is_square_valid(self, square_row, square_column):
+        """
+        Check if the 3x3 square is valid.
+        """
+        num = set()
+        for row in range(3):
+            for column in range(3):
+                val = self.grid[square_row * 3 + row][square_column * 3 + column]
+                if val in num:
+                    return False
+                if val != '_':
+                    num.add(val)
+        return True
+    
+    def is_grid_full(self):
+        for i in range(9):
+            for j in range(9):
+                if not self.grid[i][j] == '0' or '_' :
+                    print("not full grid")
+                    print(i,j)
+                    return False
+        print("full")
+        return True
+
+    def is_grid_valid(self):
+        """
+        Check if the entire grid is valid.
+        """
+        
+        for i in range(9):
+            if not self.is_row_valid(i) or not self.is_col_valid(i):
+                print("not valid column or row")
+                return False
+
+        for i in range(3):
+            for j in range(3):
+                if not self.is_square_valid(i, j):
+                    print("not valid square")
+                    return False
+        print("valid")
+        print(self.grid)
+        return True
+    
     def verifNumber(self,row, column, number):
         # Method to check entities in a row
         for i in range(0, 9):
@@ -35,57 +116,43 @@ class Sudoku:
                 if self.grid[i][j] == '0':
                     empty_spots.append((i, j))
         return empty_spots
-
-    def solveSudoku(self):
-        # Iterating through all cells of the grid
-        for row in range(9):
-            for col in range(9):
-                # If the cell is empty
-                if self.grid[row][col] == '0':
-                    # Try every possible number from 1 to 9
-                    for num in range(1, 10):
-                        num = str(num)
-                        # Place the number and continue solving
-                        self.grid[row][col] = num
-                        if self.solveSudoku():
-                            return True
-                        self.grid[row][col] = '0'
-                    # If no number works, return False
-                    return False
-        # If all cells are filled, puzzle is solved
-        
-        for row in self.grid:
-            print(' '.join(row))
-        return True
     
     def solveSudokuTest(self):
+        
+        empty_spots = self.findEmptySpots()
+        if not empty_spots:
+            # Si aucune case vide n'est trouvée, le sudoku est déjà résolu
+            return True
+
+        # Sélectionnez la première case vide
+        row, col = empty_spots[0]
+
+        # Pour chaque chiffre de 1 à 9, essayez de le placer dans la case vide
+        for num in range(1, 10):
+            self.grid[row][col] = num
+            if self.solveSudokuTest():
+                print(self.grid)
+                return True
+            # Si la solution n'est pas possible, réinitialisez la case et essayez le prochain nombre
+            self.grid[row][col] = '0'
+
+        # Si aucun nombre ne fonctionne pour cette case, le sudoku est insoluble à partir de ce point
+        return False
+
+
+    def test(self):
         empty_spot= []
         empty_spot.append(self.findEmptySpots())
-        for spots in empty_spot[0]:
-            for num in range(1,10):
-                self.grid[spots[0]][spots[1]]=str(num)
-                if self.verif.is_grid_valid():
-                    print(" c'est bon ")
-                    print(self.grid)
-                    return True
-
-            # print("c'est pas bon")
-            # for spots in empty_spot:
-            #     for num in range(1,10):
-            #         self.grid[spots[0]][spots[1]]='0'
-            # self.solveSudokuTest()
-
+        while not self.is_grid_valid():
+            for spots in empty_spot:
+                randomNumber=random.randint(1,9)
+                self.grid[empty_spot[0][spots][0]][empty_spot[0][spots][1]]= f"{randomNumber}"
+                print(random.randint(1,9))
+                print(self.grid)
+        
 
     def printGrid(self):
         print(self.grid)
-        # empty_spot= []
-        # empty_spot.append(self.findEmptySpots())
-        # print(empty_spot)
-        # for i in empty_spot[0]:
-        #     print(i[0],i[1])
-    # def SolveSudokuTest():
-    #     for i in findEmptySpots():
 
 sudoku=Sudoku()
-# sudoku.solveSudokuTest()
-sudoku.printGrid()
+sudoku.test()
